990f245641037c3b1f3c7b6a4392951925efc5b6
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-08-31 19:47:41 -0300

    scrollbar

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import Form from '@/components/Form'
 
 export default function Home() {
   return (
-    <main className='w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60 scroll-smooth'>
+    <main className='w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden'>
           <Navbar />
           <div className='w-full h-[88vh] xl:flex items-center gap-20 justify-between'>
             <div className='hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0'>
