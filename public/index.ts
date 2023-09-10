b58f5a9c2a3df5d5216ad478662d36ad5092e6e4
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-09-10 15:30:16 -0300

    PROFILE.jpg

--- public/index.ts
+++ public/index.ts
@@ -4,7 +4,7 @@ import coccions from "./assets/coccions.png";
 import bluesphere from "./assets/bluesphere.png";
 import power from "./assets/power.png";
 import boxmoment from "./assets/boxmoment.png"
-import profile from "./assets/profile.jpg";
+import profile from "./assets/profile.JPG";
 
 
 export { promptopia, snaphive, coccions, bluesphere, power, boxmoment, profile };
