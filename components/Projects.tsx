e60fafccc018b7aee3c090bcf967cd7095b9c7a0
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-07-05 19:46:49 -0300

    asas

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,9 +6,11 @@ const Projects = () => {
   return (
     <section
       id="projects"
-      className="max-w-contentContainer mx-auto px-10 lg:px-20 py-24 scroll-m-20 lg:scroll-m--[12vh]"
+      className="max-w-contentContainer mx-auto sm:px-10 lg:px-20 py-24 scroll-m-20 lg:scroll-m--[12vh]"
     >
-      <SectionTitle title="Proyectos recientes" />
+      <div className="px-6">
+        <SectionTitle title="Proyectos recientes" />
+      </div>
       <div className="w-full flex flex-col items-center justify-between gap-16 mt-5">
         {projects.map((project, index) => {
           return <ProjectCard key={index} {...project} index={index} />;
--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -4,9 +4,9 @@ interface Props {
 
 const SectionTitle = ({ title }: Props) => {
   return (
-    <h2 className="font-titleFont text-2xl font-semibold flex items-center">
+    <h2 className="font-titleFont text-2xl font-semibold flex items-center w-full">
       {title}
-      <span className="hidden sm:inline-flex md:w-60 lgl:w-72 h-[.5px] bg-gray-700 ml-6"></span>
+      <span className="hidden sm:inline-flex md:w-60 lg:w-72 h-[.5px] bg-gray-700 ml-6"></span>
     </h2>
   );
 };
